02282a0a9fd5f35c30e21520c36d3dbe31ded11b
Luchistack <ftoluchi@gmail.com>
2026-08-02 06:29:38 +0100

    stage 2: add root endpoint

--- main.py
+++ main.py
@@ -3,8 +3,27 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+tasks = [
+    {"id" : 1, "title" : "Learn FastAPI", "done" : False},
+    {"id" : 2, "title" : "Build CRUD AP", "done" : False},
+    {"id" : 3, "title" : "Push to Github", "done" : False},
+]
 # stage 1
+# @app.get("/")
+# def read_root():
+#     return {"message": "Hello Server"}
+
+
+# stage 2
 @app.get("/")
 def read_root():
-    return {"message": "Hello Server"}
+    return {"name" : "Task API",
+            "version" : "1.0",
+            "endpoints" : ["/tasks"]
+            }
 
+@app.get("/health")
+def health_check():
+    return {"status" : "ok"}
+
+#
