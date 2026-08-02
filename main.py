1b48d55dbc65112bcce2d9ad7c3449101004ff6c
Luchistack <ftoluchi@gmail.com>
2026-08-02 06:32:14 +0100

    stage 4: final project structure and updates

--- main.py
+++ main.py
@@ -39,3 +39,17 @@ def get_task(task_id: int):
             return task
     raise HTTPException(status_code=404, detail= {"error" : f"Task {task_id} not found"})
 
+# stage 4
+class TaskCreate(BaseModel):
+    title: str
+
+@app.post("/tasks", status_code =201)
+def create_task(task_data: TaskCreate):
+    if not task_data.title or not task_data.title.strip():
+        raise HTTPException(status_code=400, detail={"error" : "Title cannot be empty"})
+
+    new_id = max([t["id"] for t in tasks], default=0) + 1
+    new_task = {"id" : new_id, "title" : task_data.title.strip(), "done" : False}
+    tasks.append(new_task)
+    return new_task
+
