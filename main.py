a15b397ac04b818f1c586e5c352917506ac084d9
Luchistack <ftoluchi@gmail.com>
2026-08-02 06:31:35 +0100

    stage 3: description of what was done

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ tasks = [
     {"id" : 3, "title" : "Push to Github", "done" : False},
 ]
 # stage 1
-# @app.get("/")
-# def read_root():
-#     return {"message": "Hello Server"}
+@app.get("/")
+def read_root():
+    return {"message": "Hello Server"}
 
 
 # stage 2
@@ -26,4 +26,16 @@ def read_root():
 def health_check():
     return {"status" : "ok"}
 
-#
+# stage 3
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+    raise HTTPException(status_code=404, detail= {"error" : f"Task {task_id} not found"})
+
